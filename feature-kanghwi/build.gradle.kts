60c433c331f347db49942fb63fdf0bbc892a8844
tjrkdgnl <skh0818@hanyang.ac.kr>
2025-01-14 15:59:44 +0900

    [필수 로직 추가] - hilt gradle 추가

--- feature-kanghwi/build.gradle.kts
+++ feature-kanghwi/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.samchi.poke.feature)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -15,4 +17,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.compose.preview)
+
 }
