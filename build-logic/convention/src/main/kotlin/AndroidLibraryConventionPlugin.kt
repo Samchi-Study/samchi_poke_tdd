62cb33933e06607e2da9e10ee8d025fbd02e0f79
tjrkdgnl <skh0818@hanyang.ac.kr>
2025-01-19 14:41:05 +0900

    [kanghwi-base] - AndroidLibraryConventionPlugin 생성

---
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,33 @@
+import com.android.build.gradle.LibraryExtension
+import com.samchi.poke.samples.app.androidTestImplementation
+import com.samchi.poke.samples.app.configureKotlinAndroid
+import com.samchi.poke.samples.app.libs
+import com.samchi.poke.samples.app.testImplementation
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            extensions.configure<LibraryExtension> {
+                configureKotlinAndroid(this)
+            }
+
+            dependencies {
+                testImplementation(libs.findLibrary("junit").get())
+                testImplementation(libs.findLibrary("test-mockk").get())
+                androidTestImplementation(libs.findLibrary("androidx-junit").get())
+                androidTestImplementation(libs.findLibrary("androidx-espresso-core").get())
+            }
+        }
+    }
+}
