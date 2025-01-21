bc12ea5fa9ecfb381ebd1761219f803f94a7b1c0
서강휘 <45396949+tjrkdgnl@users.noreply.github.com>
2025-01-21 18:55:09 +0900

    [Feature] :kanghwi:data 모듈을 추가합니다. (#6)
    * [kanghwi-base-data]
    - test.core.ktx, robolectric, ext.junit.ktx 라이브러리 CustomPlugin에 추가
    
    * [kanghwi-base-data]
    - 필요한 테스트 라이브러리추가
    
    * [kanghwi-base-data]
    - network 모듈 AndroidLibraryConventionPlugin 적용
    
    * [kanghwi-base-data]
    - Retrofit Mocking 테스트

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -26,6 +26,10 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             dependencies {
                 testImplementation(libs.findLibrary("junit").get())
                 testImplementation(libs.findLibrary("test-mockk").get())
+                testImplementation(libs.findLibrary("test-core-ktx").get())
+                testImplementation(libs.findLibrary("test-ext-junit-ktx").get())
+                testImplementation(libs.findLibrary("test-robolectric").get())
+                testImplementation(libs.findLibrary("test-coroutine").get())
                 androidTestImplementation(libs.findLibrary("androidx-junit").get())
                 androidTestImplementation(libs.findLibrary("androidx-espresso-core").get())
             }
--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -1,38 +1,22 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.samchi.poke.library)
     alias(libs.plugins.samchi.poke.hilt)
     kotlin("plugin.serialization")
 }
 
 android {
     namespace = "com.samchi.poke.network"
-    compileSdk = 34
 
-    defaultConfig {
-        minSdk = 21
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
     hilt {
         enableAggregatingTask = true
     }
 }
 
 dependencies {
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.kotlin.serialization)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.network.interceptor)
+
+    testImplementation(libs.test.mock.server)
 }
