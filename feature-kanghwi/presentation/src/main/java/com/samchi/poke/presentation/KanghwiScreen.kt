5d8a1de1309a227eeebde6645e3d40489842759f
tjrkdgnl <skh0818@hanyang.ac.kr>
2025-02-02 19:41:41 +0900

    [Kanghwi-ui] - ui 수정

--- feature-kanghwi/presentation/src/main/java/com/samchi/poke/presentation/KanghwiScreen.kt
+++ feature-kanghwi/presentation/src/main/java/com/samchi/poke/presentation/KanghwiScreen.kt
@@ -1,11 +1,31 @@
 package com.samchi.poke.presentation
 
-import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImage
+import com.samchi.poke.model.Pokemon
 
 
 @Composable
@@ -20,17 +40,104 @@ internal fun KanghwiScreen(
     modifier: Modifier = Modifier,
     viewModel: KanghwiViewModel = hiltViewModel()
 ) {
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val uiState: KanghwiUiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     when (uiState) {
         is KanghwiUiState.Success -> {
-            Log.e("Success", uiState.toString())
+            PokeList(list = (uiState as KanghwiUiState.Success).pokemonList)
         }
         is KanghwiUiState.Error -> {
-            Log.e("Error", uiState.toString())
+            PokemonError {
+                viewModel.getPokeList()
+            }
         }
         KanghwiUiState.Loading -> {
-            Log.e("Loading", uiState.toString())
+            PokemonCircleProgressBar()
         }
     }
+}
+
+@Composable
+internal fun PokeList(
+    modifier: Modifier = Modifier,
+    list: List<Pokemon>
+) {
+    LazyColumn(modifier = modifier) {
+        items(list) { item ->
+            Column {
+                Row(
+                    modifier = Modifier
+                        .padding(start = 12.dp, top = 6.dp, end = 12.dp, bottom = 6.dp)
+                ) {
+                    AsyncImage(
+                        model = item.getImageUrl(),
+                        placeholder = painterResource(R.drawable.icon_pokeball_black),
+                        contentDescription = null
+                    )
+
+                    Text(
+                        modifier = modifier
+                            .padding(start = 12.dp),
+                        text = item.name
+                    )
+                }
+
+                HorizontalDivider()
+            }
+        }
+    }
+}
+
+@Composable
+internal fun PokemonError(
+    onRetryEvent: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Button(
+            modifier = Modifier
+                .align(Alignment.Center),
+            onClick = onRetryEvent,
+            shape = RoundedCornerShape(6.dp)
+        ) {
+            Text("Retry")
+        }
+    }
+}
+
+@Composable
+internal fun PokemonCircleProgressBar() {
+    Box(
+        modifier = Modifier
+            .background(Color.Transparent)
+            .fillMaxSize()
+            .clickable(enabled = false, onClick = {}),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+        )
+    }
+}
+
+
+@Composable
+@Preview
+internal fun PreviewPokeList() {
+    PokeList(
+        list = listOf(
+            Pokemon("피카츄", "https://pokeapi.co/api/v2/pokemon/2/"),
+            Pokemon("피카츄", "https://pokeapi.co/api/v2/pokemon/2/"),
+            Pokemon("피카츄", "https://pokeapi.co/api/v2/pokemon/2/"),
+            Pokemon("피카츄", "https://pokeapi.co/api/v2/pokemon/2/"),
+        )
+    )
+}
+
+@Composable
+@Preview
+internal fun PreviewError() {
+    PokemonError({})
 }
--- feature-kanghwi/presentation/src/main/java/com/samchi/poke/presentation/KanghwiViewModel.kt
+++ feature-kanghwi/presentation/src/main/java/com/samchi/poke/presentation/KanghwiViewModel.kt
@@ -7,11 +7,11 @@ import com.samchi.poke.kanghwi.common.result.asResult
 import com.samchi.poke.kanghwi.data.KanghwiRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -20,29 +20,31 @@ internal class KanghwiViewModel @Inject constructor(
     private val kanghwiRepository: KanghwiRepository
 ) : ViewModel() {
 
-    private val paginationFlow = MutableStateFlow(1)
+    private var page = 1
 
-    val uiState = paginationFlow
-        .flatMapConcat { kanghwiRepository.getPokemonInfo(offset = it) }
-        .asResult()
-        .map { result ->
-            when (result) {
-                is Result.Success -> KanghwiUiState.Success(
-                    totalCount = result.data.count,
-                    pokemonList = result.data.results
-                )
-                is Result.Error -> KanghwiUiState.Error(result.throwable)
-                Result.Loading -> KanghwiUiState.Loading
-            }
+    private val _uiState = MutableStateFlow<KanghwiUiState>(KanghwiUiState.Loading)
+    val uiState = _uiState.asStateFlow()
+
+
+    init {
+        getPokeList()
+    }
+
+    fun getPokeList() {
+        viewModelScope.launch {
+            kanghwiRepository.getPokemonInfo(offset = page)
+                .asResult()
+                .map { result ->
+                    when (result) {
+                        is Result.Success -> KanghwiUiState.Success(
+                            totalCount = result.data.count, pokemonList = result.data.results
+                        )
+                        is Result.Error -> KanghwiUiState.Error(result.throwable)
+                        Result.Loading -> KanghwiUiState.Loading
+                    }
+                }.collect {
+                    _uiState.emit(it)
+                }
         }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = KanghwiUiState.Loading
-        )
-
-
-    fun next() {
-        paginationFlow.update { it + 1 }
     }
 }
