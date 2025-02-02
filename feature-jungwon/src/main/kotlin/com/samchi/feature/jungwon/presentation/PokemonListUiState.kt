0d0e676d716a1409abeba72be7ea7e4d29c6eb29
JungWon, Kim <renovatio0424@gmail.com>
2025-02-02 21:07:21 +0900

    [feat] PokemonListViewModel 테스트 코드 작성 & 구현
    - PokemonListViewModel 테스트 케이스 추가
    - 초기 상태 검증
    - 로딩 상태 검증
    - 데이터 로드 성공/실패 검증
    - 페이지네이션 검증
    - 새로고침 기능 검증

--- feature-jungwon/src/main/kotlin/com/samchi/feature/jungwon/presentation/PokemonListUiState.kt
+++ feature-jungwon/src/main/kotlin/com/samchi/feature/jungwon/presentation/PokemonListUiState.kt
@@ -3,8 +3,8 @@ package com.samchi.feature.jungwon.presentation
 import com.samchi.feature.jungwon.data.model.PokemonPage
 
 sealed class PokemonListUiState {
-    object Initial : PokemonListUiState()
-    object Loading : PokemonListUiState()
+    data object Initial : PokemonListUiState()
+    data object Loading : PokemonListUiState()
     data class Success(val data: PokemonPage) : PokemonListUiState()
     data class Error(val message: String) : PokemonListUiState()
 }
