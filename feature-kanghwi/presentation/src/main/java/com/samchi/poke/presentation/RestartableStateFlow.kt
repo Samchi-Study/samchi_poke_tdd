509cbd5ad8dc32de02276962bd3a4bcc83fb7b68
tjrkdgnl <skh0818@hanyang.ac.kr>
2025-02-03 17:37:38 +0900

    [Kanghwi-ui] - RestartableStateFlow 생성

---
+++ feature-kanghwi/presentation/src/main/java/com/samchi/poke/presentation/RestartableStateFlow.kt
@@ -0,0 +1,30 @@
+package com.samchi.poke.presentation
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+
+
+interface RestartableStateFlow<T> : StateFlow<T> {
+    fun restart()
+}
+
+
+fun <T> Flow<T>.restartableStateIn(
+    scope: CoroutineScope,
+    sharingStarted: SharingStarted,
+    initialValue: T
+): RestartableStateFlow<T> {
+    val sharingRestartableImpl = SharingRestartableImpl(sharingStarted)
+    val stateFlow = stateIn(
+        scope = scope,
+        started = sharingRestartableImpl,
+        initialValue = initialValue
+    )
+
+    return object : RestartableStateFlow<T>, StateFlow<T> by stateFlow {
+        override fun restart() = sharingRestartableImpl.restart()
+    }
+}
