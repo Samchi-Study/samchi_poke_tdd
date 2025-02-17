dde3542785e2534a85c28e836b642678e8ab0799
최우성/Inapp서비스개발팀/원스토어 <H202003084@hycu.ac.kr>
2025-02-17 15:31:23 +0900

    [feature][woosung] paging 3 관련 코드 추가

---
+++ feature-woosung/src/test/java/com/samchi/feature/woosung/FakeWoosungPokeRepositoryImp.kt
@@ -0,0 +1,16 @@
+package com.samchi.feature.woosung
+
+import com.samchi.feature.woosung.data.repository.WoosungPokeRepository
+import com.samchi.poke.model.Pokemon
+
+//class FakeWoosungPokeRepositoryImp : WoosungPokeRepository {
+//    override suspend fun getPokemonList(): List<Pokemon> = listOf(
+//        fakePokemon
+//    )
+//}
+
+
+val fakePokemon = Pokemon(
+    name = "bulbasaur",
+    url = "https://pokeapi.co/api/v2/pokemon/1/"
+)
