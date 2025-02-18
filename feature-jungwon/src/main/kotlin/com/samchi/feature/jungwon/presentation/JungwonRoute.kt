ee53ee8f6465c9218048e4d2eb2d76abafb231cc
JungWon, Kim <renovatio0424@gmail.com>
2025-02-18 21:51:59 +0900

    [refactor] pokemon_list 패키지 명 list 로 변경

--- feature-jungwon/src/main/kotlin/com/samchi/feature/jungwon/presentation/JungwonRoute.kt
+++ feature-jungwon/src/main/kotlin/com/samchi/feature/jungwon/presentation/JungwonRoute.kt
@@ -2,7 +2,7 @@ package com.samchi.feature.jungwon.presentation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.samchi.feature.jungwon.presentation.pokemon_list.PokemonListScreen
+import com.samchi.feature.jungwon.presentation.list.PokemonListScreen
 
 @Composable
 fun JungwonRoute(
