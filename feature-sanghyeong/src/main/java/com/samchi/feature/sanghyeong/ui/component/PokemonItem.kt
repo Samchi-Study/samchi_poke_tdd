cd110f12b43ffe690505271a7c5cb3c0eeae0c7d
shwoo-wev <sang.woo@weversecompany.com>
2025-03-16 23:07:24 +0900

    [상형-Feature] 3주차 - Room 구현

--- feature-sanghyeong/src/main/java/com/samchi/feature/sanghyeong/ui/component/PokemonItem.kt
+++ feature-sanghyeong/src/main/java/com/samchi/feature/sanghyeong/ui/component/PokemonItem.kt
@@ -1,8 +1,9 @@
 package com.samchi.feature.sanghyeong.ui.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,33 +14,56 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
-import com.samchi.poke.model.Pokemon
+import com.samchi.feature.sanghyeong.R
+import com.samchi.feature.sanghyeong.model.SangHyeongPokemon
 
 @Composable
-internal fun PokemonItem(pokemon: Pokemon) {
+internal fun PokemonItem(
+    pokemon: SangHyeongPokemon,
+    onFavoriteClick: (SangHyeongPokemon) -> Unit,
+) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .background(color = Color.LightGray, shape = RoundedCornerShape(size = 20.dp))
-            .padding(all = 25.dp),
+            .padding(
+                vertical = 25.dp,
+                horizontal = 40.dp,
+            ),
         horizontalArrangement = Arrangement.spacedBy(space = 20.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         AsyncImage(
-            modifier = Modifier.size(150.dp),
+            modifier = Modifier.size(100.dp),
             model = pokemon.getImageUrl(),
             contentDescription = null
         )
 
         Text(
+            modifier = Modifier.weight(1f),
             text = pokemon.name,
-            fontWeight = FontWeight.W400,
-            fontSize = 16.sp,
+            fontSize = 20.sp,
+        )
+
+        Image(
+            modifier = Modifier
+                .size(24.dp)
+                .clickable {
+                    onFavoriteClick.invoke(pokemon)
+                },
+            painter = painterResource(
+                id = if (pokemon.isFavorite) {
+                    R.drawable.ic_favorite_filled
+                } else {
+                    R.drawable.ic_favorite_border
+                }
+            ),
+            contentDescription = null,
         )
     }
 }
@@ -47,5 +71,12 @@ internal fun PokemonItem(pokemon: Pokemon) {
 @Preview
 @Composable
 private fun PokemonItemPreview() {
-    PokemonItem(pokemon = Pokemon(name = "test", url = ""))
+    PokemonItem(
+        pokemon = SangHyeongPokemon(
+            name = "test",
+            url = "",
+            isFavorite = false,
+        ),
+        onFavoriteClick = { },
+    )
 }
