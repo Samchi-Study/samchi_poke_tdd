db13313aa99e2f79bcc5c063a9b543e6fe555ec9
최우성/Inapp서비스개발팀/원스토어 <vov1252@naver.com>
2025-04-08 20:21:43 +0900

    [woosung] Confilt 해결

--- feature-woosung/src/main/java/com/samchi/feature/woosung/data/repository/WoosungPokeRepositoryImp.kt
+++ feature-woosung/src/main/java/com/samchi/feature/woosung/data/repository/WoosungPokeRepositoryImp.kt
@@ -5,6 +5,7 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.samchi.feature.woosung.data.remote.WoosungPokeRemoteDataSource
 import com.samchi.feature.woosung.data.paging.WoosungPagingSource
+import com.samchi.feature.woosung.data.remote.WoosungPokeRemoteDataSource
 import com.samchi.poke.model.Pokemon
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
