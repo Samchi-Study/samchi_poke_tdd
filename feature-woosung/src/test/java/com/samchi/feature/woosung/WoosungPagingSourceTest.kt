f9ac89fb05dd807b77e0eee9f051e96789e3af5b
최우성/Inapp서비스개발팀/원스토어 <vov1252@naver.com>
2025-04-08 20:21:43 +0900

    [woosung] Confilt 해결

--- feature-woosung/src/test/java/com/samchi/feature/woosung/WoosungPagingSourceTest.kt
+++ feature-woosung/src/test/java/com/samchi/feature/woosung/WoosungPagingSourceTest.kt
@@ -3,8 +3,8 @@ package com.samchi.feature.woosung
 import androidx.paging.PagingConfig
 import androidx.paging.PagingSource
 import androidx.paging.testing.TestPager
-import com.samchi.feature.woosung.data.datasource.WoosungPokeRemoteDataSource
 import com.samchi.feature.woosung.data.paging.WoosungPagingSource
+import com.samchi.feature.woosung.data.remote.WoosungPokeRemoteDataSource
 import com.samchi.feature.woosung.mapper.toPokemon
 import com.samchi.poke.network.dto.ResponsePokemon
 import com.samchi.poke.network.dto.ResponsePokemonInfo
