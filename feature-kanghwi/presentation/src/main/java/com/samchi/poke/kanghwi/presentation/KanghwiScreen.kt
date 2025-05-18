6a592df06b9982bd85fc0d4ab4abb6a5bcaa4de0
seoganghwi <skh0818@hanyang.ac.kr>
2025-05-18 15:45:58 +0900

    textStyle 수정

--- feature-kanghwi/presentation/src/main/java/com/samchi/poke/kanghwi/presentation/KanghwiScreen.kt
+++ feature-kanghwi/presentation/src/main/java/com/samchi/poke/kanghwi/presentation/KanghwiScreen.kt
@@ -24,31 +24,23 @@ import androidx.compose.material3.SnackbarResult.ActionPerformed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.SnapshotMutationPolicy
-import androidx.compose.runtime.State
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
-import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.DefaultAlpha
-import androidx.compose.ui.graphics.FilterQuality
-import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultFilterQuality
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -57,16 +49,13 @@ import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.palette.graphics.Palette
-import coil3.SingletonImageLoader
-import coil3.compose.AsyncImage
-import coil3.compose.AsyncImagePainter
-import coil3.compose.LocalPlatformContext
-import coil3.request.ImageRequest
-import coil3.request.allowHardware
 import coil3.toBitmap
 import com.samchi.poke.kanghwi.model.Pokemon
+import com.samchi.poke.kanghwi.presentation.ui.KanghwiAsyncImage
 import com.samchi.poke.kanghwi.presentation.ui.KanghwiPokeTddTheme
 import com.samchi.poke.kanghwi.presentation.ui.KanghwiPokeTheme
+import com.samchi.poke.kanghwi.presentation.ui.getBackgroundColor
+import com.samchi.poke.kanghwi.presentation.ui.rememberPaletteState
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -234,7 +223,11 @@ private fun PokemonItem(
                 modifier = modifier
                     .padding(vertical = 6.dp),
                 text = pokemon.name,
-                color = Color.White
+                color = KanghwiPokeTddTheme.colors.dark,
+                style = TextStyle(
+                    letterSpacing = 0.6.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
             )
         }
     }
@@ -251,70 +244,6 @@ private fun PokeLoadingBar() {
         CircularProgressIndicator(
             modifier = Modifier.padding(0.dp, 4.dp)
         )
-    }
-}
-
-@Composable
-fun KanghwiAsyncImage(
-    url: String,
-    contentDescription: String?,
-    modifier: Modifier = Modifier,
-    placeholder: Painter? = null,
-    error: Painter? = null,
-    fallback: Painter? = error,
-    onLoading: ((AsyncImagePainter.State.Loading) -> Unit)? = null,
-    onSuccess: ((AsyncImagePainter.State.Success) -> Unit)? = null,
-    onError: ((AsyncImagePainter.State.Error) -> Unit)? = null,
-    alignment: Alignment = Alignment.Center,
-    contentScale: ContentScale = ContentScale.Fit,
-    alpha: Float = DefaultAlpha,
-    colorFilter: ColorFilter? = null,
-    filterQuality: FilterQuality = DefaultFilterQuality,
-    clipToBounds: Boolean = true,
-) = AsyncImage(
-    model = ImageRequest.Builder(LocalContext.current)
-        .data(url)
-        .allowHardware(false)
-        .build(),
-    contentDescription = contentDescription,
-    imageLoader = SingletonImageLoader.get(LocalPlatformContext.current),
-    modifier = modifier,
-    placeholder = placeholder,
-    error = error,
-    fallback = fallback,
-    onLoading = onLoading,
-    onSuccess = onSuccess,
-    onError = onError,
-    alignment = alignment,
-    contentScale = contentScale,
-    alpha = alpha,
-    colorFilter = colorFilter,
-    filterQuality = filterQuality,
-    clipToBounds = clipToBounds,
-)
-
-@Composable
-private fun rememberPaletteState(
-    value: Palette? = null,
-    policy: SnapshotMutationPolicy<Palette?> = structuralEqualityPolicy(),
-): MutableState<Palette?> = remember(key1 = value) {
-    mutableStateOf(value = value, policy = policy)
-}
-
-@Composable
-fun Palette?.getBackgroundColor(): State<Color> {
-    val defaultColor = KanghwiPokeTddTheme.backgroundColor
-
-    return remember(this) {
-        derivedStateOf {
-            val rgb = this?.dominantSwatch?.rgb
-
-            if (rgb != null) {
-                Color(rgb)
-            } else {
-                defaultColor
-            }
-        }
     }
 }
 
